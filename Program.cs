3cba05e0a6b21a0c5c0291b311691bbb9fcfc5de
Adrian Rupp <adrian.rupp@ingentis.com>
2019-11-13 09:22:43 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Apitron.PDF.Kit;
+
+namespace apitron_dotnetcore3_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an empty document
+            var document = new FixedDocument();
+            using var outStream = File.OpenWrite("empty-file.pdf");
+
+            // and save it without any changes
+            document.Save(outStream);
+
+
+
+
+            // open an existing pdf file
+            using var inStream = File.OpenRead("../../../sample-files/test.pdf");
+            var document2 = new FixedDocument(inStream);
+
+            // and saving it without any changes
+            using var outStream2 = File.OpenWrite("copy-file.pdf");
+            document2.Save(outStream2);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+Run `dotnet build` in the project root
+
+Afterwards run `dontet apitron-dotnetcore3-sample.dll` found in `bin/Debug/netcoreapp3.0`
